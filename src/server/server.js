0c584467226aca56226f7460d93e98b4c7796de2
yaelBrown <yaelrbrown@gmail.com>
2021-12-31 22:57:05 -0600

    feat: integrated json-server with express

--- src/server/server.js
+++ src/server/server.js
@@ -3,26 +3,13 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const app = express()
 const router = express.Router()
+const middlewares = jsonServer.defaults()
 
-server.use()
 
 // Test route
 app.get("/test", (req, res) => res.send({"msg": "hello"}))
 
+// API
+app.use('/api', jsonServer.router('db/db.json'))
+
 app.listen(3000)
-
-
-/*
-var express = require('express');
-var jsonServer = require('json-server');
-
-var server = express();
-// ...
-
-// You may want to mount JSON Server on a specific end-point, for example /api
-// Optiona,l except if you want to have JSON Server defaults
-// server.use('/api', jsonServer.defaults()); 
-server.use('/api', jsonServer.router('db.json'));
-
-server.listen(3000);
-*/
